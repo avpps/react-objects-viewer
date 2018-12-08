b0b60f6c5b80f3b70c13b81beaeb9efcd7abc5b9
Piotr <piotrallegr@gmail.com>
2018-12-08 23:56:25 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     let buttons = []
-
     for (let i=0; i<100; i++) {
       buttons.push(
         <button className="buttonHis">12:00:00:000 aaa_bbb_ccc</button>
       )
     }
+
+    let buttonsDet = []
+    for (let i=0; i<100; i++) {
+      buttonsDet.push(
+        <button className="buttonDet">aaa-bbb-ccc-ddd-eee</button>
+      )
+    }
     return (
       <div className="divMain">
-        <div className="divHeader">
-          <button className="buttonMode">MODE</button>
-          <button className="buttonLt">FFFF</button>
-          <button className="buttonLt">DDDD</button>
-          <button className="buttonLt">BBBB</button>
-        </div>
-        <div className="divWork">
-          <div className="divInput">
+        <div className="divInp">
+          <div className="divHea">
+            <button className="buttonMode">MODE</button>
+          </div>
+          <div className="divTxa">
             <textarea className="textareaInput"/>
           </div>
-          <div className="divHistory">
+        </div>
+        <div className="divSum">
+          <div className="divHea">
+            <button className="buttonLt">FFFF</button>
+            <button className="buttonLt">DDDD</button>
+            <button className="buttonLt">BBBB</button>
+          </div>
+          <div className="divBut">
             <div className="divHistoryAll">
               <button className="buttonAll">ALL</button>
             </div>
@@ -30,7 +41,17 @@ class App extends Component {
               {buttons}
             </div>
           </div>
-          <div className="divOutput">
+        </div>
+        <div className="divDet">
+          <div className="divDetHea">
+            <div className="divDetHeaBut">
+              {buttonsDet}
+            </div>
+            <div className="divDetHeaAll">
+              <button className="buttonAll">All</button>
+            </div>
+          </div>
+          <div className="divDetTxa">
             <textarea className="textareaOutput"/>
           </div>
         </div>
