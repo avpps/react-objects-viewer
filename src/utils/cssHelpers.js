0e4a7e564dbc15987f225aa43558f3b5525acfcf
Piotrek <piotrallegr@gmail.com>
2019-03-02 21:55:03 +0100

    Styles moving to JS code ....

---
+++ src/utils/cssHelpers.js
@@ -0,0 +1,11 @@
+export function UnselectableOptions () {
+
+  return {
+    userSelect: 'none',
+    msUserSelect: 'none',
+    mozUserSelect: 'none',
+    khtmlUserSelect: 'none',
+    webkitUserSelect: 'none',
+    webkitTouchCallout: 'none',
+  }
+}
